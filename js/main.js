3161a88a80141eb497a07f2711fc39568fcc4a88
NikitaTsepenkovIT <79404059+NikitaTsepenkovIT@users.noreply.github.com>
2021-04-09 20:18:05 +0500

    adapt1

--- js/main.js
+++ js/main.js
@@ -1,7 +1,25 @@
 const swiper = new Swiper('.channel-slider', {
     // Optional parameters
     loop: true,
-    slidesPerView: 6,
+    slidesPerView: 1,
+    spaceBetween: 20,
+    breakpoints: {
+      1900: {
+        slidesPerView: 6
+      },
+      1600: {
+        slidesPerView: 5
+      },
+      1300: {
+        slidesPerView: 4
+      },
+      1100: {
+        slidesPerView: 3
+      },
+      800: {
+        slidesPerView: 2
+      },
+    },
     // Navigation arrows
     navigation: {
       nextEl: '.channel-button-next',
@@ -13,7 +31,19 @@ const swiper = new Swiper('.channel-slider', {
   const swiperRec = new Swiper('.channel-slider-rec', {
     // Optional parameters
     loop: true,
-    slidesPerView: 6,
+    slidesPerView: 1,
+    spaceBetween: 20,
+    breakpoints: {
+      1600: {
+        slidesPerView: 3
+      },
+      1100: {
+        slidesPerView: 2
+      },
+      800: {
+        slidesPerView: 1
+      },
+    },
     // Navigation arrows
     navigation: {
       nextEl: '.channel-button-next-rec',
@@ -25,7 +55,25 @@ const swiper = new Swiper('.channel-slider', {
   const swiperFd = new Swiper('.channel-slider-fd', {
     // Optional parameters
     loop: true,
-    slidesPerView: 6,
+    slidesPerView: 1,
+    spaceBetween: 20,
+    breakpoints: {
+      1900: {
+        slidesPerView: 6
+      },
+      1600: {
+        slidesPerView: 5
+      },
+      1300: {
+        slidesPerView: 4
+      },
+      1100: {
+        slidesPerView: 3
+      },
+      800: {
+        slidesPerView: 2
+      },
+    },
     // Navigation arrows
     navigation: {
       nextEl: '.channel-button-next-fd',
@@ -33,3 +81,16 @@ const swiper = new Swiper('.channel-slider', {
     },
   
   });
+
+  const searchBtn = document.querySelector ('.mobile-search')
+  const mobileSearch = document.querySelector ('.input-group')
+  searchBtn.addEventListener('click', () => {
+     mobileSearch.classList.toggle('is-open');
+
+  });
+
+  if(document.documentElement.scrollWidth <= 640) {
+    swiper.destroy();
+    swiperRec.destroy();
+    swiperFd.destroy();
+  }
