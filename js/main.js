a48485403edb48b94b0c6bfb88769fb18730aa56
NikitaTsepenkovIT <79404059+NikitaTsepenkovIT@users.noreply.github.com>
2021-04-09 16:24:40 +0500

    Fourth

--- js/main.js
+++ js/main.js
@@ -2,12 +2,34 @@ const swiper = new Swiper('.channel-slider', {
     // Optional parameters
     loop: true,
     slidesPerView: 6,
-  
-  
     // Navigation arrows
     navigation: {
       nextEl: '.channel-button-next',
       prevEl: '.channel-button-prev',
+    },
+  
+  });
+  
+  const swiperRec = new Swiper('.channel-slider-rec', {
+    // Optional parameters
+    loop: true,
+    slidesPerView: 6,
+    // Navigation arrows
+    navigation: {
+      nextEl: '.channel-button-next-rec',
+      prevEl: '.channel-button-prev-rec',
+    },
+  
+  });
+
+  const swiperFd = new Swiper('.channel-slider-fd', {
+    // Optional parameters
+    loop: true,
+    slidesPerView: 6,
+    // Navigation arrows
+    navigation: {
+      nextEl: '.channel-button-next-fd',
+      prevEl: '.channel-button-prev-fd',
     },
   
   });
